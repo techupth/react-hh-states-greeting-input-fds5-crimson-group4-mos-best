0f5666340daccfab326f11f7b8f2f242f004b8e2
Mos amon <amonchanchai@gmail.com>
2023-12-21 15:26:26 +0700

    feat:completeReactGreetingInputEx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+//set reactstate
+// ให้ Greeting Message เปลี่ยนได้
+// โดยเปลี่ยนเป็น input message
+// เอา input มาใช้แบบในคลิป
+// ให้อัพเดทเมื่อคลิ้ก
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [greetingInputText, setgreetingInputText] = useState("");
+  const [greetingText, setgreetingText] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setgreetingInputText(event.target.value);
+          }}
+          value={greetingInputText}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setgreetingText(greetingInputText);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
